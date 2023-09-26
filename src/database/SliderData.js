b460b986a1810f6be480ff38c0087e0f99dc0272
Tomáš Zeleňák <tomaszelenak8@gmail.com>
2023-09-26 21:36:49 +0200

    data changes

--- src/database/SliderData.js
+++ src/database/SliderData.js
@@ -2,26 +2,26 @@ export const SliderData = [
     {
         id: 1,
         title: 'Brown Valley',
-        linkImg: 'public/images/SliderImages/sliderMap1.png',
+        linkImg: '/images/SliderImages/sliderMap1.png',
     },
     {
         id: 2,
         title: 'Red Village',
-        linkImg: 'public/images/SliderImages/sliderMap2.png',
+        linkImg: '/images/SliderImages/sliderMap2.png',
     },
     {
         id: 3,
         title: 'Red Village',
-        linkImg: 'public/images/SliderImages/sliderMap3.png',
+        linkImg: '/images/SliderImages/sliderMap3.png',
     },
     {
         id: 4,
         title: 'China',
-        linkImg: 'public/images/SliderImages/sliderMap4.png',
+        linkImg: '/images/SliderImages/sliderMap4.png',
     },
     {
         id: 5,
         title: 'Yellow Islands',
-        linkImg: 'public/images/SliderImages/sliderMap5.png',
+        linkImg: '/images/SliderImages/sliderMap5.png',
     },
 ]
